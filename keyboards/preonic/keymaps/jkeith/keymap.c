d5668c48e60dffcf5d1bbbbd474624228953fb65
jimmykodes <jkeith@kochava.com>
2022-06-30 15:39:39 -0700

    feat: add macros

--- keyboards/preonic/keymaps/jkeith/keymap.c
+++ keyboards/preonic/keymaps/jkeith/keymap.c
@@ -42,11 +42,11 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_5x12(
-  KC_ESC,  NUMS,    MEDIA, FKEYS, KC_4,    KC_5,       KC_6,   KC_7,          KC_8,      KC_9,          KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,  KC_E,  KC_R,    KC_T,       KC_Y,   KC_U,          KC_I,      KC_O,          KC_P,    SHORTCUTS,
-  ARROWS,  KC_A,    KC_S,  KC_D,  KC_F,    KC_G,       KC_H,   TD(TD_JJ_ESC), KC_K,      KC_L,          KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,    KC_B,       KC_N,   KC_M,          KC_COMM,   KC_DOT,        KC_SLSH, SYMBOLS,
-  KC_LCTL, KC_LALT, EMOJI, NAV,   KC_LGUI, S(KC_LGUI), KC_ENT, KC_SPC,        A(KC_ENT), OSM(MOD_HYPR), KC_CAPS, LOCK
+  KC_ESC,  NUMS,    MEDIA,   FKEYS, KC_4,    KC_5,       KC_6,   KC_7,          KC_8,      KC_9,          KC_0,    KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,       KC_Y,   KC_U,          KC_I,      KC_O,          KC_P,    SHORTCUTS,
+  ARROWS,  KC_A,    KC_S,    KC_D,  KC_F,    KC_G,       KC_H,   TD(TD_JJ_ESC), KC_K,      KC_L,          KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,       KC_N,   KC_M,          KC_COMM,   KC_DOT,        KC_SLSH, SYMBOLS,
+  KC_LCTL, KC_LALT, DM_PLY1, NAV,   KC_LGUI, S(KC_LGUI), KC_ENT, KC_SPC,        A(KC_ENT), OSM(MOD_HYPR), DM_REC1, LOCK
 ),
 [_UBU_BASE] = LAYOUT_ortho_5x12(
   KC_ESC,  NUMS,    MEDIA,   FKEYS, KC_4,    KC_5,       KC_6,   KC_7,          KC_8,      KC_9,         KC_0,    KC_BSPC,
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SYMBOLS] = LAYOUT_ortho_5x12(
     _______, KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
     _______, KC_PERC, KC_PLUS,  KC_LCBR, KC_RCBR, FT_ARR,  CLN_EQ,   KC_EQL,  KC_AMPR, _______, _______, _______,
-    _______, KC_UNDS, KC_MINUS, KC_LPRN, KC_RPRN, KC_GRV,  KC_TILDE, KC_DLR,  KC_PIPE, KC_BSLS, _______, _______,
+    EMOJI,   KC_UNDS, KC_MINUS, KC_LPRN, KC_RPRN, KC_GRV,  KC_TILDE, KC_DLR,  KC_PIPE, KC_BSLS, _______, _______,
     _______, _______, _______,  KC_LBRC, KC_RBRC, _______, _______,  _______, L_ARR,   R_ARR,   _______, _______,
     _______, _______, _______,  _______, _______, _______, _______,  _______, _______, _______, _______, _______
 ),
--- keyboards/preonic/keymaps/jkeith/rules.mk
+++ keyboards/preonic/keymaps/jkeith/rules.mk
@@ -1,3 +1,4 @@
 SRC += muse.c
 
 TAP_DANCE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
