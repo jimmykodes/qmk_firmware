f204da140ab75382010641c9053e84525b787af9
jimmykodes <jimmy@khanacademy.org>
2023-10-27 12:40:11 -0700

    feat: refactor thumbs

--- keyboards/preonic/keymaps/jkeith/keymap.c
+++ keyboards/preonic/keymaps/jkeith/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        /*  |  */  KC_Y,    KC_U,    KC_I,          KC_O,    KC_P,    _______,
     ARROWS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        /*  |  */  KC_H,    KC_J,    KC_K,          KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        /*  |  */  KC_N,    KC_M,    KC_COMM,       KC_DOT,  KC_SLSH, SYMBOLS,
-    KC_LCTL, KC_LALT, _______, _______, KC_LGUI, S(KC_LGUI),  /*  |  */  KC_ENT,  KC_SPC,  OSM(MOD_HYPR), _______, _______, LOCK
+    KC_LALT, _______, _______, KC_LCTL, KC_LGUI, S(KC_LGUI),  /*  |  */  KC_ENT,  KC_SPC,  OSM(MOD_HYPR), _______, _______, LOCK
 ),
 [_SYMBOLS] = LAYOUT_ortho_5x12(
 //  < ESC >, <  1  >, <  2  >,  <  3  >, <  4  >, <  5  >,  /*  |  */  <  6  >,  <  7  >, <  8  >, <  9  >, <  0  >, < BKSP >
