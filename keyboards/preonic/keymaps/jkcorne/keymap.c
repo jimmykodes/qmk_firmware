29e878191bc96a48bb0eb5fdd3adbf9829819e52
jimmykodes <jimmy@khanacademy.org>
2024-01-08 08:33:11 -0800

    fix: return s(lgui) to thumb

--- keyboards/preonic/keymaps/jkcorne/keymap.c
+++ keyboards/preonic/keymaps/jkcorne/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     SYSTEM,  NUMS,    MEDIA,   KC_E,    KC_R,    KC_T,     /*  |  */  KC_Y,    KC_U,    KC_I,          KC_O,    KC_P,    KC_BSPC,
     ARROWS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     /*  |  */  KC_H,    KC_J,    KC_K,          KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     /*  |  */  KC_N,    KC_M,    KC_COMM,       KC_DOT,  KC_SLSH, SYMBOLS,
-    _______, _______, KC_LALT, KC_LCTL, KC_LGUI, _______,  /*  |  */  NUMROW,  KC_SPC,  OSM(MOD_HYPR), _______, _______, _______
+    _______, _______, KC_LALT, KC_LCTL, KC_LGUI, S(KC_LGUI),  /*  |  */  NUMROW,  KC_SPC,  OSM(MOD_HYPR), _______, _______, _______
 ),
 [_SYMBOLS] = LAYOUT_ortho_5x12(
 //  < ESC >, <  1  >, <  2  >,  <  3  >, <  4  >, <  5  >,  /*  |  */  <  6  >,  <  7  >, <  8  >, <  9  >, <  0  >, < BKSP >
